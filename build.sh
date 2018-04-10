134e2627ac3632cf99a3a12615e03451a905b3c9
wes <wes@home.local>
2018-04-09 22:17:53 -0500

    Breaking out builds

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker build -t wesparish/xmr-stack:2.2.0-1.0 . && \
-  docker push wesparish/xmr-stack:2.2.0-1.0
+docker build -t wesparish/xmr-stack:amd-2.2.0-1.1 ./amd && \
+  docker push wesparish/xmr-stack:amd-2.2.0-1.1
+
+docker build -t wesparish/xmr-stack:nvidia-2.2.0-1.1 ./nvidia && \
+  docker push wesparish/xmr-stack:nvidia-2.2.0-1.1
